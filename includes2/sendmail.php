2c6e26b847c1f291e2d0bc4e6697858acd6865f4
James Boom Boom Bamptini <33868129+bamptini@users.noreply.github.com>
2025-05-22 11:41:32 +0100

    Add files via upload

---
+++ includes2/sendmail.php
@@ -0,0 +1,22 @@
+<?php
+
+if(isset($_POST['submit'])){
+	$to = "bamptini@hotmail.com"; // Your email address
+	$name = $_POST['name'];
+	$from = $_POST['email'];
+	$phone = $_POST['mobile'];
+	$message = $_POST['message'];
+	$subject = "Contact Form Details";
+	$headers = "From:" . $from;
+	$result = mail($to,$subject,$message,$headers);
+
+	if ($result) {
+		echo '<script type="text/javascript">alert("Your Message was sent Successfully!");</script>';
+		echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+
+	}else{
+		echo '<script type="text/javascript">alert("Sorry! Message was NOT sent, Try again Later.");</script>';
+		echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+	}
+}
+?>
